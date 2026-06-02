90e210272253e3f9a7ca63229864d9616e0ab2fb
Abdelrahman Aboulfotouh <aboelfotouhcp2023@gmail.com>
2026-06-02 23:11:44 +0300

    Add spring data JDBC , SPring Web and POstgreSQL driver dependencies

---
+++ src/main/java/org/example/capitbackend/CapItBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.capitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapItBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapItBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/capitbackend/CapItBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.capitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapItBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
