aa66b3d9a01d1d339a0ba2db16d821afe2c3da25
Abdelrahman Aboulfotouh <aboelfotouhcp2023@gmail.com>
2026-06-04 02:55:59 +0300

    fix : Bug: New User Registration Does Not Persist User Record When UUID Is Assigned Before Save

--- src/main/java/org/example/capitbackend/model/User.java
+++ src/main/java/org/example/capitbackend/model/User.java
@@ -4,10 +4,12 @@
             import java.util.UUID;
             import org.springframework.data.annotation.Id;
             import org.springframework.data.annotation.ReadOnlyProperty;
+            import org.springframework.data.annotation.Transient;
+            import org.springframework.data.domain.Persistable;
             import org.springframework.data.relational.core.mapping.Table;
 
 @Table("users")
-public class User {
+public class User  implements Persistable<UUID> {
 
     @Id
     private UUID id;
@@ -28,9 +30,15 @@ public class User {
 
     private OffsetDateTime lastActiveAt;
 
-
+    @Override
     public UUID getId() {
         return id;
+    }
+    @Transient
+    private boolean isNew = true;
+    @Override
+    public boolean isNew() {
+        return isNew;
     }
 
     public void setId(UUID id) {
